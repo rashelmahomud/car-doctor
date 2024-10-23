06b59b30ddb3d8c0ccd19d4b8fbd2950dbe4128c
MCTC <rashelmahmudraj1998@gmail.com>
2024-10-23 16:31:58 +0600

    added update

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,9 @@
 const loading = () => {
   return (
     <div>
-      <p className="text-5xl text-primary">Loading.....</p>
+      <p className="text-5xl text-primary text-center">
+        <progress className="progress w-56"></progress>
+      </p>
     </div>
   );
 };
---
+++ src/app/services/page.jsx
@@ -0,0 +1,12 @@
+import Services from "@/components/home/Services";
+import React from "react";
+
+const ServicePage = () => {
+  return (
+    <div>
+      <Services />
+    </div>
+  );
+};
+
+export default ServicePage;
